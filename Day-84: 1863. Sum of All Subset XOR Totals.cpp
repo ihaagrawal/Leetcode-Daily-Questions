0b5158e3f63faa9f3cefd87d4e557652bd4de81f
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-05-20 12:03:40 +0530

    Create Day-84: 1863. Sum of All Subset XOR Totals.cpp

---
+++ Day-84: 1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,14 @@
+// 1863. Sum of All Subset XOR Totals (EASY)
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/?envType=daily-question&envId=2024-05-20
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int sumTotal = 0;
+
+        for (int num : nums) {
+            sumTotal |= num;
+        }
+        return sumTotal << (nums.size() - 1);
+    }
+};
