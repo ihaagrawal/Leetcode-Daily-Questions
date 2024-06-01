864ca8ae565c4af59e0b66f96aca9530745df731
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-01 06:15:00 +0530

    Create Day-96: 3110. Score of a String.cpp

---
+++ Day-96: 3110. Score of a String.cpp
@@ -0,0 +1,14 @@
+// 3110. Score of a String (EASY)
+// https://leetcode.com/problems/score-of-a-string/description/?envType=daily-question&envId=2024-06-01
+
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n=s.length(), sum=0;
+        for(int i=1;i<n;i++){
+            sum += abs(s[i]-s[i-1]);
+        }
+        return sum;
+    }
+};
