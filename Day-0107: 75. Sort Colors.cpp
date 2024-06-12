5136253a359889b6ee47f680a91577e4efa9b18c
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-12 09:08:49 +0530

    Create Day-0107: 75. Sort Colors.cpp

---
+++ Day-0107: 75. Sort Colors.cpp
@@ -0,0 +1,23 @@
+// 75. Sort Colors (MEDIUM)
+// https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2024-06-12
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int i=0, left=0, right=n-1;
+        while(i<=right){
+            if(nums[i]==0){
+                swap(nums[i], nums[left]);
+                left++;
+                i++;
+            }
+            else if(nums[i]==2){
+                swap(nums[i], nums[right]);
+                right--;
+            }
+            else i++;
+        }
+    }
+};
