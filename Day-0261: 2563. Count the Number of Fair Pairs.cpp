788c338ae2a9d918e762fcb794454eb8ca38fc86
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-11-13 22:44:02 +0530

    Create Day-0261: 2563. Count the Number of Fair Pairs.cpp

---
+++ Day-0261: 2563. Count the Number of Fair Pairs.cpp
@@ -0,0 +1,19 @@
+// 2563. Count the Number of Fair Pairs (MEDIUM)
+// https://leetcode.com/problems/count-the-number-of-fair-pairs/description/?envType=daily-question&envId=2024-11-13
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long count = 0;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            auto h = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]);
+            auto l = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
+            count += (h - l);
+        }
+
+        return count;
+    }
+};
