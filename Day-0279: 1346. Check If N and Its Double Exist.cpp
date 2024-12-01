37cf3a8f78768c7e22e3752f0603b6f03905f77f
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-12-01 09:34:10 +0530

    Create Day-0279: 1346. Check If N and Its Double Exist.cpp

---
+++ Day-0279: 1346. Check If N and Its Double Exist.cpp
@@ -0,0 +1,15 @@
+// 1346. Check If N and Its Double Exist (EASY)
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/description/?envType=daily-question&envId=2024-12-01
+
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> st;
+        for(int num : arr){
+            if(st.count(num * 2) || (num % 2 == 0 && st.count(num / 2))) return true;
+            st.insert(num);
+        }
+        return false;
+    }
+};
