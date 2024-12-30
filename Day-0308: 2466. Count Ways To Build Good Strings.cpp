4a295e36c844cbdb9c2e06b0ca7c42db508c00ad
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-12-30 14:17:23 +0530

    Create Day-0308: 2466. Count Ways To Build Good Strings.cpp

---
+++ Day-0308: 2466. Count Ways To Build Good Strings.cpp
@@ -0,0 +1,24 @@
+// 2466. Count Ways To Build Good Strings (MEDIUM)
+// https://leetcode.com/problems/count-ways-to-build-good-strings/description/?envType=daily-question&envId=2024-12-30
+
+
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        const int MOD = 1e9 + 7;
+        vector<int> dp(high + 1, 0);
+        dp[0] = 1; 
+        
+        for (int i = 1; i <= high; ++i) {
+            if (i >= zero) dp[i] = (dp[i] + dp[i - zero]) % MOD;
+            if (i >= one) dp[i] = (dp[i] + dp[i - one]) % MOD;
+        }
+
+        int result = 0;
+        for (int i = low; i <= high; ++i) {
+            result = (result + dp[i]) % MOD;
+        }
+
+        return result;
+    }
+};
